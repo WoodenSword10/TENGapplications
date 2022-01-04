9585831bcad15530f2136c5e92d147cc3ebf03f6
WoodenSword10 <1205793031@qq.com>
2022-01-05 00:30:12 +0800

    添加注释，尝试优化接收数据逻辑

--- TENG.py
+++ TENG.py
@@ -13,7 +13,11 @@ from PyQt5.QtCore import QThread, pyqtSignal, QMutex, Qt, QDateTime, QTimer
 import windows
 sensor_data = []
 
+# 保存放大模块数据
 class save_Data_txt(QThread):
+    '''
+    保存数据
+    '''
     def __init__(self):
         super(save_Data_txt, self).__init__()
 
@@ -25,142 +29,257 @@ class save_Data_txt(QThread):
                     files.write('\n')
                 QThread.msleep(10000)
 
+# 定时刷新串口列表
 class Find_port(QThread):
+    '''
+    寻找存在的串口
+    '''
+    # 串口改变信号
     port_change = pyqtSignal()
 
     def __init__(self):
         super(Find_port, self).__init__()
+        # 获取初始串口列表
         self.old = list(serial.tools.list_ports.comports())
 
     def run(self):
         while True:
+            # 再次获取串口列表
             port_list = list(serial.tools.list_ports.comports())
+            # 如果串口列表发生了变化
             if port_list != self.old:
+                # 更改初始串口列表
                 self.old = port_list
+                # 发出信号
                 self.port_change.emit()
+                # 强制休息1000毫秒
                 QThread.msleep(1000)
 
+# 接收放大模块的数据
 class recv_data(QThread):
     '''
     放大模块数据的获取
     '''
+    # 接收数据信号
     update_pb = pyqtSignal(int)
+    # 空串口对象，用于接收指定串口
     port = serial.Serial()
+    # 运行标志
     is_run = False
     def __init__(self):
         super(recv_data, self).__init__()
 
     def change_port(self, port, is_run):
+        '''
+        改变串口及运行标志
+        :param port: 串口
+        :param is_run: 运行标志
+        :return:
+        '''
         self.port = port
         self.is_run = is_run
 
     def run(self):
+        '''
+        线程运行函数
+        :return:
+        '''
         while self.is_run:
             try:
+                # 尝试读取数据
                 dat = self.port.readline()
             except:
+                # 读取失败
                 print('--')
                 continue
             else:
+                # 读取成功
                 try:
+                    # 将数据转换为字符串
                     dat = str(dat, encoding="utf-8")
                 except:
+                    # 转换失败
                     print('---')
                     continue
                 else:
+                    # 转换成功，进行模式匹配
                     pattern = re.compile('[\d]*')
+                    # 找出第一个数字
                     dat = pattern.findall(dat)[0]
+                    # 如果数字长度不为0
                     if len(dat) > 0:
+                        # 将数字转换为整型
                         dat = int(dat)
+                        # 发出更新数据信号
                         self.update_pb.emit(dat)
+                        # 将数据加入到sensor_data
                         sensor_data.append(dat)
                         # print('放大模块：', dat)
 
+# 接收arduino单片机的数据
 class recv_data_2(QThread):
     '''
     arduino板子数据的获取
     '''
+    # arduino数据接收信号
     re_data = pyqtSignal(str, int)
+    # 空串口
     port = serial.Serial()
+    # 运行标志
     is_run = False
     def __init__(self):
         super(recv_data_2, self).__init__()
 
     def change_port(self, port, is_open):
+        '''
+        改变串口和运行标志
+        :param port: 串口
+        :param is_open: 运行标志
+        :return:
+        '''
         self.port = port
         self.is_run = is_open
 
     def change_state(self, is_run):
+        '''
+        运行标志转换
+        :param is_run:运行标志
+        :return:
+        '''
         self.is_run = is_run
 
     def run(self):
+        '''
+        线程执行函数
+        :return:
+        '''
         while True:
+            # 如果运行标志为True
             if self.is_run:
                 try:
+                    # 尝试读取数据
                     data = self.port.readline()
                 except:
+                    # 报错空过
                     # print('---')
                     continue
                 else:
+                    # 数据处理
                     # print(data, len(data), type(data))
+                    # 将数据转化为字符串
                     data = str(data, encoding="utf-8")
+                    # 发出arduino接收数据信号
                     self.re_data.emit(data, 0)
                     print('arduino: ', data)
             else:
+                # 强制睡眠线程1000毫秒
                 QThread.msleep(2000)
+                # 转换执行标志为True
                 self.is_run = True
         pass
 
+# 开始界面
 class beginWindow(QMainWindow, beginui.Ui_Form):
+    '''
+    串口设置界面设置
+    '''
+    # 用于发送arduino串口号的信号
     mySignal1 = pyqtSignal(str)
+    # 用于发送放大模块串口号的信号
     mySignal2 = pyqtSignal(str)
+    # 放大模块串口号是否打开标志
     is_port_open = False
     def __init__(self, parent=None):
+        # 窗口初始化
         super(beginWindow, self).__init__(parent)
         self.setupUi(self)
         # 获取存在的端口号
         self.port_find()
+        # 实例化保存文件线程
         self.thread4 = save_Data_txt()
+        # 线程开启
         self.thread4.start()
+        # 实例化端口号变化线程
         self.thread3 = Find_port()
+        # 线程内置串口改变信号链接函数
         self.thread3.port_change.connect(self.port_find)
+        # 线程开始
         self.thread3.start()
+        # 主界面实例化
         self.My_win = MyWindow()
+        # 放大模块数据显示界面实例化
         self.Port_win = portWindow()
+        # arduino数据读取线程信号链接函数
         self.My_win.thread1.re_data.connect(self.decision)
+        #
         self.Port_win.mySignal3.connect(self.decision)
+        # arduino串口号信号处理函数
         self.mySignal1.connect(self.My_win.get_port)
+        # 放大模块串口号信号处理函数
         self.mySignal2.connect(self.Port_win.getBeginSignal)
+
         self.Port_win.mySignal3.connect(self.My_win.getPortData)
+
         self.Port_win.mySignal3.connect(self.My_win.change_color)
+        # 设置按钮1点击触发函数，实现打开密码锁界面
         self.pushButton.clicked.connect(self.port_conn)
+        # 设置按钮2点击触发函数，实现打开放大模块数据显示界面
         self.pushButton_2.clicked.connect(self.port_plot)
+        # 获取当前时间
         self.old_time = time.time()
 
+    # 串口刷新显示函数
     def port_find(self):
+        '''
+        定时刷新显示串口列表
+        :return:
+        '''
+        # 清空两个复选框
         self.comboBox.clear()
         self.comboBox_2.clear()
+        # 用于存放处理后串口列表
         list1 = []
+        # 获取串口列表
         port_list = list(serial.tools.list_ports.comports())
+        # 对串口列表进行处理，仅保留COMxx
         for item in port_list:
+            # 使用正则匹配
             pattern = re.compile('COM[\d]*')
+            # 找出符号要求的字符串
             item = pattern.findall(str(item))[0]
+            # 添加入列表中
             list1.append(item)
+        # 对列表进行排序
         list1.sort()
+        # 填充复选框内容
         self.comboBox.addItems(list1)
         self.comboBox_2.addItems(list1)
 
+    # 串口连接函数
     def port_conn(self):
+        '''
+        连接串口
+        :return:
+        '''
+        # 将复选框1中的当前内容通过信号发送出去，即arduino模块串口号
         self.mySignal1.emit(self.comboBox.currentText())
+        # 如果存在自定义密码则获取自定义密码
         if len(self.lineEdit.text()) != 0:
             self.My_win.password = self.lineEdit.text()
+        # 如果串口还未打开
         if not self.is_port_open:
+            # 将串口打开标志置为True
             self.is_port_open = True
+            # 将复选框2中的当前内容通过信号发送出去，即放大模块串口号
             self.mySignal2.emit(self.comboBox_2.currentText())
+            # 将放大模块数据显示界面中的单选框置为选中状态
             self.Port_win.checkBox.setChecked(True)
+        # 展示密码锁界面
         self.My_win.show()
+        # 隐藏当前页面
         self.hide()
 
+    # 处理触发的按钮
     def decision(self, data, i):
         new_time = time.time()
         if new_time - self.old_time > 1:
@@ -170,11 +289,21 @@ class beginWindow(QMainWindow, beginui.Ui_Form):
                 self.My_win.recv_data(data)
         self.old_time = time.time()
 
+    # 放大模块数据显示界面的开启
     def port_plot(self):
+        '''
+
+        :return:
+        '''
+        # 如果串口打开标志为假
         if not self.is_port_open:
+            # 将串口打开标志置为真
             self.is_port_open = True
+            # 通过信号发送放大模块串口号
             self.mySignal2.emit(self.comboBox_2.currentText())
+            # 将放大模块数据显示界面的单选框置为选中状态
             self.Port_win.checkBox.setChecked(True)
+        # 展示放大模块数据显示界面
         self.Port_win.show()
         pass
 
